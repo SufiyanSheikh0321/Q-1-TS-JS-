0245f92fb0117ce016d4e575304e841b0a75e96e
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 16:17:47 +0500

    Add files via upload

---
+++ Q.1.js
@@ -0,0 +1,10 @@
+"use strict";
+//  1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+Object.defineProperty(exports, "__esModule", { value: true });
+//Task
+1.; //https://code.visualstudio.com/download for (vs code)
+// 2. nodejs.org/en/download for (nodejs)
+// 3. npm i -g typescript  for (typescript install key)
+// 4. tsc -- init  for (tsconfig.json file key)
+// 5.npm init -y for (package lock.json key)
+// 6.npm i @types/node -D for (package_lock.json key type)
